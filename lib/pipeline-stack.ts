ca3e23084023c2397685102ac51fb094afc7645e
Rico Huijbers <rix0rrr@gmail.com>
2026-06-12 15:53:10 +0200

    Add credential to prevent throttling

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,19 +1,30 @@
 import * as cdk from 'aws-cdk-lib';
 import * as cbuild from 'aws-cdk-lib/aws-codebuild';
 import { Construct } from 'constructs';
-import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { CodePipeline, CodePipelineSource, DockerCredential, ShellStep } from 'aws-cdk-lib/pipelines';
 import { ScenarioStage } from './scenario';
 import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { Secret } from 'aws-cdk-lib/aws-secretsmanager';
 
 export class PipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const dockerHubSecret = Secret.fromSecretCompleteArn(
+      this,
+      'DHSecret',
+      'arn:aws:secretsmanager:us-east-1:355421412380:secret:dockerhub-token-xuxzIG'
+    );
 
     const pipeline = new CodePipeline(this, 'Pipeline', {
       // The pipeline name
       pipelineName: 'TestingPipeline',
 
       crossAccountKeys: true,
+
+      dockerCredentials: [
+        DockerCredential.dockerHub(dockerHubSecret),
+      ],
 
       codeBuildDefaults: {
         buildEnvironment: {
