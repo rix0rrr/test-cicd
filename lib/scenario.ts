4ed757c85cafd7c7d79f981fe1f581b716009c99
Rico Huijbers <rix0rrr@gmail.com>
2026-06-12 12:58:46 +0200

    chore: different scenario

--- lib/scenario.ts
+++ lib/scenario.ts
@@ -31,8 +31,8 @@ export class ScenarioStack extends cdk.Stack {
 
     // new InvalidResourceProps(this, 'Props');
     // new AutoImportWrongRetention(this, 'AutoImportWrongRetention')
-    new CircularDependencies(this, 'Circs', { vpc });
-    // new EcsExitOnStartup(this, 'ExitOnStartup', { cluster });
+    // new CircularDependencies(this, 'Circs', { vpc });
+    new EcsExitOnStartup(this, 'ExitOnStartup', { cluster });
     // new EcsHealthCheckFailure(this, 'HealthCheck', { cluster, vpc });
     // new EcsImagePullFailure(this, 'ImagePull', { cluster });
     // new EcsNoCwLogging(this, 'NoLogging', { cluster });
